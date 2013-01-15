11a02a3266770d0f30fb015058b818f0553f97f3
Nick Kugaevsky <nick@kugaevsky.ru>
2013-01-15 15:15:09 +0400

    Load LDAP settings from yaml file

--- lib/jackad/config.rb
+++ lib/jackad/config.rb
@@ -1,7 +1,7 @@
 module Jackad
   class Config
     def self.setup
-      @@options ||= YAML.load_file('/usr/local/jackad.yml')
+      YAML.load_file('/usr/local/jackad/ldap.yml')
     end
   end
 end
