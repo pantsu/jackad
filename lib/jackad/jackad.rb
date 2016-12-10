7e77fb64d45f10014c6ded961e48d10fba2b7b7e
Nick Kugaevsky <nick@kugaevsky.ru>
2016-12-10 14:43:22 +0300

    Typofix

--- lib/jackad/jackad.rb
+++ lib/jackad/jackad.rb
@@ -26,7 +26,6 @@ module Jackad
     filter_by_uac = ~Net::LDAP::Filter.construct('useraccountcontrol:1.2.840.113556.1.4.803:=2')
     filter_by_pass = ~Net::LDAP::Filter.eq('pwdlastset', '0')
     filter = filter_by_attr & filter_by_uac & filter_by_pass
-    ad.ldap.search(filter: filter, size: 1).size > 0 ?
+    ad.ldap.search(filter: filter, size: 1).size > 0
   end
-
 end
